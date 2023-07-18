d4e633d55aec162721766fb388fe57863a26364c
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-07-17 19:13:44 -0700

    Update comment-seeds.js

--- comment-seeds.js
+++ comment-seeds.js
@@ -2,19 +2,16 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    comment_text: "Cool",
-    user_id: 1,
-    post_id: 1
+    comment_text: "Great",
+   
   },
   {
-    comment_text: "Good to know",
-    user_id: 2,
-    post_id: 2
+    comment_text: "Nice",
+    
   },
   {
-    comment_text: "Awesome",
-    user_id: 3,
-    post_id: 3
+    comment_text: "Sick",
+    
   }
 ];
 
